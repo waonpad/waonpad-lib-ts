ada53eb348e4afb9b70b8d0303770e024c62f574
waonpad <waonpad@gmail.com>
2024-10-29 02:09:13 +0900

    ^^

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [],
   test: {
     globals: false,
-    include: ["src/**/*.test.{ts}"],
+    include: ["src/**/*.test.ts"],
     alias: {
       "@": resolve(__dirname, "./src"),
     },
